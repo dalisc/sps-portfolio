356bd9bc71decc2bb2cb7790fb6392f3ce38816a
Dalis Chan <dalischan97@gmail.com>
2020-06-03 23:04:55 +0800

    Customise HTML in portfolio page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,35 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+/**
+ * Adds a random greeting to the page.
+ */
+function addRandomMeme() {
+  const imgSources =
+      ['https://i.chzbgr.com/full/9340630528/hFC589711/spotify-playlist-songs-about-git',
+      'https://www.probytes.net/wp-content/uploads/2018/01/3.jpg!',
+      'https://scontent-xsp1-1.xx.fbcdn.net/v/t1.15752-9/59408853_2016918848436701_5907180139245993984_n.png?_nc_cat=110&_nc_sid=b96e70&_nc_ohc=faMQj0KFoPwAX8eRmA1&_nc_ht=scontent-xsp1-1.xx&oh=cf34b166e78195d2efd71eb1446126d6&oe=5EFEE7F2',
+      'https://qph.fs.quoracdn.net/main-qimg-50da7dacced9bcc11c246394388fc911.webp',
+      'https://miro.medium.com/max/3200/0*FL03kQRXT_TFerSD',
+      'https://cdn-images-1.medium.com/max/1600/1*OVo5mxqgkAs8x8rZloCD8A.png'];
+
+  // Pick a random url.
+  const imgSource = imgSources[Math.floor(Math.random() * imgSources.length)];
+
+  // Create an image element
+  var img = document.createElement('img');
+  img.src = imgSource;
+  img.alt = "meme";
+  img.width = "300";
+
+  // Remove the current image, if any
+  const memeContainer = document.getElementById('meme-container');
+  if (memeContainer.firstChild) {
+      memeContainer.removeChild(memeContainer.firstChild);
+  }
+
+  // Add it to the page.
+  memeContainer.appendChild(img);
+}
+
