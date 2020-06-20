46747e09b34933a19972772e8499a34c0e530bc3
Dalis Chan <dalischan97@gmail.com>
2020-06-20 20:56:36 +0800

    Add form validation

---
+++ portfolio/src/main/webapp/assets/js/formValidation.js
@@ -0,0 +1,39 @@
+function validateMarkerForm() {
+  var lat, lng, link;
+  var latError, lngError, linkError;
+
+  const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/;
+
+  lat = document.forms["mapForm"]["lat"].value;
+  lng = document.forms["mapForm"]["lng"].value;
+  link = document.forms["mapForm"]["link"].value;
+
+  console.log(lat, lng, link)
+
+  // If x is Not a Number or less than one or greater than 10
+  if (lat < -90 || lat > 90) {
+    document.getElementById("lat-error").innerHTML = "Latitude must be between -90 and 90.";
+    latError = true;
+  } else {
+    document.getElementById("lat-error").innerHTML = "";
+    latError = false;
+  }
+
+  if (lng < -180 || lng > 80) {
+    document.getElementById("lng-error").innerHTML = "Latitude must be between -180 and 80.";
+    lngError = true;
+  } else {
+    document.getElementById("lng-error").innerHTML = "";
+    lngError = false;
+  }
+
+  if (!urlRegex.test(link)) {
+    document.getElementById("link-error").innerHTML = "Not a valid URL. Did you specify the protocol (HTTP or HTTPS)?";
+    linkError = true;
+  } else {
+    document.getElementById("link-error").innerHTML = "";
+    linkError = false;
+  }
+
+  return !latError && !lngError && !linkError;
+}
