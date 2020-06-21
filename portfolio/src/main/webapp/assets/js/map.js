3ea88f6bc5bfd73fac15dafae117c3d65a5bc2cb
Dalis Chan <dalischan97@gmail.com>
2020-06-21 10:09:48 +0800

    Add quick zoom in/out when users click on markers

--- portfolio/src/main/webapp/assets/js/map.js
+++ portfolio/src/main/webapp/assets/js/map.js
@@ -1,344 +1,354 @@
-(function (exports) {
-  "use strict";
+(function(exports) {
+    "use strict";
 
-  var mapCustomStyles = [
-    {
-      elementType: "geometry",
-      stylers: [
+    var mapCustomStyles = [
         {
-          color: "#ebe3cd",
-        },
-      ],
-    },
-    {
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#523735",
-        },
-      ],
-    },
-    {
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#f5f1e6",
-        },
-      ],
-    },
-    {
-      featureType: "administrative",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#c9b2a6",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#bab387",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#ebe3cd",
+                },
+            ],
         },
         {
-          weight: 1.5,
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "labels",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#4f4040",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#523735",
+                },
+            ],
         },
         {
-          weight: 0.5,
+            elementType: "labels.text.stroke",
+            stylers: [
+                {
+                    color: "#f5f1e6",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "landscape.natural",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#f3c623",
+            featureType: "administrative",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#c9b2a6",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#dfd2ae",
+            featureType: "administrative.land_parcel",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#bab387",
+                },
+                {
+                    weight: 1.5,
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "labels.text.fill",
-      stylers: [
         {
-          color: "#93817c",
+            featureType: "administrative.land_parcel",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "off",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "geometry.fill",
-      stylers: [
         {
-          color: "#7d9461",
+            featureType: "administrative.land_parcel",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#4f4040",
+                },
+                {
+                    weight: 0.5,
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "labels.text.fill",
-      stylers: [
         {
-          color: "#2c691b",
+            featureType: "landscape.natural",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#f3c623",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#f5f1e6",
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.arterial",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#f3d3d3",
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#93817c",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.arterial",
-      elementType: "labels",
-      stylers: [
         {
-          visibility: "off",
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [
+                {
+                    color: "#7d9461",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#f8c967",
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#2c691b",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry.stroke",
-      stylers: [
         {
-          color: "#c8a55f",
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#f5f1e6",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels",
-      stylers: [
         {
-          visibility: "off",
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#f3d3d3",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.highway.controlled_access",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#e98d58",
+            featureType: "road.arterial",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "off",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.highway.controlled_access",
-      elementType: "geometry.stroke",
-      stylers: [
         {
-          color: "#ecc6b1",
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#f8c967",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.local",
-      stylers: [
         {
-          visibility: "off",
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#c8a55f",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.local",
-      elementType: "labels",
-      stylers: [
         {
-          visibility: "off",
+            featureType: "road.highway",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "off",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "road.local",
-      elementType: "labels.text.fill",
-      stylers: [
         {
-          color: "#aa8f83",
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#e98d58",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "transit.line",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#dfd2ae",
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry.stroke",
+            stylers: [
+                {
+                    color: "#ecc6b1",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "transit.line",
-      elementType: "labels.text.fill",
-      stylers: [
         {
-          color: "#8f7d77",
+            featureType: "road.local",
+            stylers: [
+                {
+                    visibility: "off",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "transit.line",
-      elementType: "labels.text.stroke",
-      stylers: [
         {
-          color: "#ebe3cd",
+            featureType: "road.local",
+            elementType: "labels",
+            stylers: [
+                {
+                    visibility: "off",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "transit.station",
-      elementType: "geometry",
-      stylers: [
         {
-          color: "#dfd2ae",
+            featureType: "road.local",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#aa8f83",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "water",
-      elementType: "geometry.fill",
-      stylers: [
         {
-          color: "#127681",
+            featureType: "transit.line",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
         },
-      ],
-    },
-    {
-      featureType: "water",
-      elementType: "labels.text.fill",
-      stylers: [
         {
-          color: "#92998d",
+            featureType: "transit.line",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#8f7d77",
+                },
+            ],
         },
-      ],
-    },
-  ];
+        {
+            featureType: "transit.line",
+            elementType: "labels.text.stroke",
+            stylers: [
+                {
+                    color: "#ebe3cd",
+                },
+            ],
+        },
+        {
+            featureType: "transit.station",
+            elementType: "geometry",
+            stylers: [
+                {
+                    color: "#dfd2ae",
+                },
+            ],
+        },
+        {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [
+                {
+                    color: "#127681",
+                },
+            ],
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [
+                {
+                    color: "#92998d",
+                },
+            ],
+        },
+    ];
 
-  var markers = [];
+    var markers = [];
+    var map;
 
-  function initMap() {
-    exports.map = new google.maps.Map(document.getElementById("map"), {
-      center: {
-        lat: 1.3521,
-        lng: 103.8198,
-      },
-      zoom: 2,
-      styles: mapCustomStyles,
-    });
+    function initMap() {
+        exports.map = new google.maps.Map(document.getElementById("map"), {
+            center: {
+                lat: 1.3521,
+                lng: 103.8198,
+            },
+            zoom: 2,
+            styles: mapCustomStyles,
+        });
 
-    var map = exports.map;
-    var infowindow = new google.maps.InfoWindow();
+        map = exports.map;
+        var infowindow = new google.maps.InfoWindow();
 
-    createMarkers(map, infowindow);
-  }
+        createMarkers(map, infowindow);
+    }
 
-  function createMarkers(map, infowindow) {
-    fetch("/markers")
-      .then((response) => response.json())
-      .then((markers) => {
-        console.log(markers);
-        for (var i = 0; i < markers.length; i++) {
-          createMarkerWithInfoWindow(map, infowindow, markers[i]);
-        }
-      });
-  }
 
-  function createMarkerWithInfoWindow(map, infowindow, place) {
-    var content = parseInfowindowContent(place);
+    function createMarkers(map, infowindow) {
+        fetch("/markers")
+            .then((response) => response.json())
+            .then((markers) => {
+                for (var i = 0; i < markers.length; i++) {
+                    createMarkerWithInfoWindow(map, infowindow, markers[i]);
+                }
+            });
+    }
 
-    var marker = new google.maps.Marker({
-      map: map,
-      animation: google.maps.Animation.DROP,
-      position: { lat: place.lat, lng: place.lng },
-      title: place.title,
-    });
+    function createMarkerWithInfoWindow(map, infowindow, place) {
+        var content = parseInfowindowContent(place);
 
-    google.maps.event.addListener(
-      marker,
-      "click",
-      (function (marker, content, infowindow) {
-        return function () {
-          infowindow.setContent(content);
-          infowindow.open(map, marker);
-        };
-      })(marker, content, infowindow)
-    );
+        var marker = new google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
+            position: { lat: place.lat, lng: place.lng },
+            title: place.title,
+        });
 
-    markers.push(marker);
-  }
+        google.maps.event.addListener(
+            marker,
+            "click",
+            (function(map, marker, content, infowindow) {
+                return function() {
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                    map.setZoom(13);
+                    map.setCenter(marker.getPosition());
+                };
+            })(map, marker, content, infowindow)
+        );
 
-  function parseInfowindowContent(place) {
-    var contentString =
-      '<div id="infoWindow">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      `<h1 id="infoWindowHeading">${place.title}</h1>` +
-      '<div id="infowindowContent">' +
-      `<p style="white-space: pre-line">${place.desc}</p>` +
-      `<p><a style="display: ${place.link ? "inline-block" : "none"};" href="${
-        place.link
-      }" target="_blank">Discover more!</a></p>` +
-      `<p style="display: ${
-        place.submitter ? "inline-block" : "none"
-      }; font-style: italic;">Submitted by: ${place.submitter}</p>` +
-      "</div>" +
-      "</div>";
+        markers.push(marker);
+    }
 
-    return contentString;
-  }
 
-  exports.initMap = initMap;
+    function parseInfowindowContent(place) {
+        var contentString =
+            '<div id="infoWindow">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            `<h1 id="infoWindowHeading">${place.title}</h1>` +
+            '<div id="infowindowContent">' +
+            `<p style="white-space: pre-line">${place.desc}</p>` +
+            `<p><a style="display: ${place.link ? "inline-block" : "none"};" href="${
+            place.link
+            }" target="_blank">Discover more!</a></p>` +
+            `<p style="display: ${
+            place.submitter ? "inline-block" : "none"
+            }; font-style: italic;">Submitted by: ${place.submitter}</p>` +
+            "</div>" +
+            "</div>";
+
+        return contentString;
+    }
+
+    exports.initMap = initMap;
+
 })((this.window = this.window || {}));
+
+
+function zoomOut() {
+    map.setZoom(2);
+}
--- portfolio/src/main/webapp/assets/js/script.js
+++ portfolio/src/main/webapp/assets/js/script.js
@@ -16,99 +16,102 @@
  * Creates images for photography gallery
  */
 
-document.addEventListener("DOMContentLoaded", function () {
-  addFilmImages();
-  getComments();
+document.addEventListener("DOMContentLoaded", function() {
+    addFilmImages();
+    getComments();
 });
 
+function zoomOut() {
+    console.log("trigger");
+}
 function addFilmImages() {
-  const fileNames = [
-    "1-dalis.jpg",
-    "1-dog1.jpg",
-    "1-valley1.jpg",
-    "1-dog2.jpg",
-    "1-fog.jpg",
-    "1-halo.jpg",
-    "1-mountains.jpg",
-    "1-houses2.jpg",
-    "1-people.jpg",
-    "1-silhouette.jpg",
-    "1-sunrise.jpg",
-    "2-bridge.jpg",
-    "2-farm.jpg",
-    "2-horse.jpg",
-    "2-sky1.jpg",
-    "2-forest.jpg",
-    "2-silhouette2.jpg",
-    "2-sky2.jpg",
-    "2-sunrise2.jpg",
-    "3-plane.jpg",
-    "3-waterfall.jpg",
-    "3-cliff.jpg",
-    "3-mountain.jpg",
-    "3-wave.jpg",
-    "3-boats.jpg",
-    "4-vivocity2.jpg",
-    "4-vivocity3.jpg",
-    "4-vivocity1.jpg",
-    "4-jewel3.jpg",
-    "4-lightleak.jpg",
-    "4-jewel1.jpg",
-    "4-jewel5.jpg",
-    "4-jewel2.jpg",
-    "4-jewel4.jpg",
-    "4-mrt.jpg",
-    "4-universitytown.jpg",
-    "5-dexposed1.jpg",
-    "5-dexposed10.jpg",
-    "5-dexposed2.jpg",
-    "5-dexposed3.jpg",
-    "5-dexposed4.jpg",
-    "5-dexposed5.jpg",
-    "5-dexposed6.jpg",
-    "5-dexposed8.jpg",
-    "5-dexposed9.jpg",
-  ];
+    const fileNames = [
+        "1-dalis.jpg",
+        "1-dog1.jpg",
+        "1-valley1.jpg",
+        "1-dog2.jpg",
+        "1-fog.jpg",
+        "1-halo.jpg",
+        "1-mountains.jpg",
+        "1-houses2.jpg",
+        "1-people.jpg",
+        "1-silhouette.jpg",
+        "1-sunrise.jpg",
+        "2-bridge.jpg",
+        "2-farm.jpg",
+        "2-horse.jpg",
+        "2-sky1.jpg",
+        "2-forest.jpg",
+        "2-silhouette2.jpg",
+        "2-sky2.jpg",
+        "2-sunrise2.jpg",
+        "3-plane.jpg",
+        "3-waterfall.jpg",
+        "3-cliff.jpg",
+        "3-mountain.jpg",
+        "3-wave.jpg",
+        "3-boats.jpg",
+        "4-vivocity2.jpg",
+        "4-vivocity3.jpg",
+        "4-vivocity1.jpg",
+        "4-jewel3.jpg",
+        "4-lightleak.jpg",
+        "4-jewel1.jpg",
+        "4-jewel5.jpg",
+        "4-jewel2.jpg",
+        "4-jewel4.jpg",
+        "4-mrt.jpg",
+        "4-universitytown.jpg",
+        "5-dexposed1.jpg",
+        "5-dexposed10.jpg",
+        "5-dexposed2.jpg",
+        "5-dexposed3.jpg",
+        "5-dexposed4.jpg",
+        "5-dexposed5.jpg",
+        "5-dexposed6.jpg",
+        "5-dexposed8.jpg",
+        "5-dexposed9.jpg",
+    ];
 
-  const imagesContainer = document.getElementById("image-gallery-container");
+    const imagesContainer = document.getElementById("image-gallery-container");
 
-  fileNames.forEach((fileName) => {
-    // Create a wrapper for zool colorize effect
-    var wrapper = document.createElement("div");
-    wrapper.className = "img-hover-zoom img-hover-zoom-gallery";
-    // Create an image element
-    var img = document.createElement("img");
-    img.src = "images/photography/" + fileName;
-    img.alt = fileName;
-    img.class = "image-gallery-item";
-    // Put the image in the wrapper
-    wrapper.appendChild(img);
-    // Append the image wrapper to the container.
-    imagesContainer.appendChild(wrapper);
-  });
+    fileNames.forEach((fileName) => {
+        // Create a wrapper for zool colorize effect
+        var wrapper = document.createElement("div");
+        wrapper.className = "img-hover-zoom img-hover-zoom-gallery";
+        // Create an image element
+        var img = document.createElement("img");
+        img.src = "images/photography/" + fileName;
+        img.alt = fileName;
+        img.class = "image-gallery-item";
+        // Put the image in the wrapper
+        wrapper.appendChild(img);
+        // Append the image wrapper to the container.
+        imagesContainer.appendChild(wrapper);
+    });
 }
 
 function toggleImageGallery() {
-  var gallery = document.getElementById("image-gallery-wrapper");
-  var toggleButton = document.getElementById("view-gallery-button");
-  if (gallery.style.display === "none") {
-    gallery.style.display = "block";
-    toggleButton.innerHTML = "close gallery";
-  } else {
-    gallery.style.display = "none";
-    toggleButton.innerHTML = "view image gallery";
-  }
+    var gallery = document.getElementById("image-gallery-wrapper");
+    var toggleButton = document.getElementById("view-gallery-button");
+    if (gallery.style.display === "none") {
+        gallery.style.display = "block";
+        toggleButton.innerHTML = "close gallery";
+    } else {
+        gallery.style.display = "none";
+        toggleButton.innerHTML = "view image gallery";
+    }
 }
 
 function getComments() {
-  fetch("/comments")
-    .then((response) => response.json())
-    .then((comments) => {
-      const commentsContainer = document.getElementById("comments-container");
-      comments.map((comment) =>
-        commentsContainer.appendChild(createCommentSection(comment))
-      );
-    });
+    fetch("/comments")
+        .then((response) => response.json())
+        .then((comments) => {
+            const commentsContainer = document.getElementById("comments-container");
+            comments.map((comment) =>
+                commentsContainer.appendChild(createCommentSection(comment))
+            );
+        });
 }
 
 /** Creates some HTML that displays the comments 
@@ -130,10 +133,15 @@ function getComments() {
     </div>
 */
 function createCommentSection(comment) {
-  const commentWrapper = document.createElement("div");
-  commentWrapper.id = "comment-wrapper";
-  commentWrapper.innerHTML =
-    `<div id="comment-header"> <p id="username-display">${comment.username}</p> <p id="timestamp-display">${comment.timestamp}</p> </div> <p id="comments-display">${comment.comments}</p> <hr class="solid" />`;
-    
-  return commentWrapper;
+    const commentWrapper = document.createElement("div");
+    commentWrapper.id = "comment-wrapper";
+    commentWrapper.innerHTML =
+        `<div id="comment-header">
+            <p id="username-display">${comment.username}</p>
+            <p id="timestamp-display">${comment.timestamp}</p>
+        </div> 
+        <p id="comments-display">${comment.comments}</p>
+        <hr class="solid" />`;
+
+    return commentWrapper;
 }
